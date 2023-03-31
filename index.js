7f85887cfda1cfda1048f10d7b5b62171d1dc625
xNapha <nathancai1997@gmail.com>
2023-03-31 20:04:26 +1100

    bug fixing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import currentTime from "./scripts/dateTime.js";
 import { apps } from "./data/appData.js";
 import { addToDock } from "./scripts/dock.js";
 import { renderToolBar } from "./scripts/toolbar.js";
-import { toolBarData } from "./data/toolBarData.js";
+import { toolBarData } from "./data/toolbarData.js";
 // run clock function
 currentTime();
 //  run clock function at every 1000 milliseconds
